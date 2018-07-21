32fb93b01c03f62244cb1191051045e68d5518d4
Carbinol <carbinol.index@qq.com>
2018-07-21 09:40:12 +0800

    edit editpage

--- CMDCalendar/CMDCalendar/EditPage.xaml.cs
+++ CMDCalendar/CMDCalendar/EditPage.xaml.cs
@@ -13,8 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-using BasicMvvm.Models;
-using BasicMvvm.ViewModels;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -22,49 +20,14 @@ namespace BasicMvvm {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page {
-        public MainPage() {
+    public sealed partial class EditPage : Page {
+        public EditPage() {
             this.InitializeComponent();
         }
 
-        // 绕过点击AppBarButton不会导致TextBox失去焦点问题的方法。
-        /*private void LoseFocusAppBarButton_OnClick(object sender,
-            RoutedEventArgs e) {
-            var textbox = FocusManager.GetFocusedElement() as TextBox;
-            if (textbox != null) {
-                var binding =
-                    textbox.GetBindingExpression(TextBox.TextProperty);
-                binding.UpdateSource();
-            }
-        }*/
-
-        private void AdaptiveStates_OnCurrentStateChanged(object sender,
-            VisualStateChangedEventArgs e) {
-            UpdateForVisualState(e.NewState, e.OldState);
-        }
-
-        private void UpdateForVisualState(VisualState newState,
-            VisualState oldState = null) {
-            var viewModel = (MainPageViewModel) this.DataContext;
-
-            var isNarrow = newState == NarrowState;
-
-            if (isNarrow && oldState == DefaultState &&
-                viewModel.SelectedContact != null) {
-                Frame.Navigate(typeof(DetailPage), null,
-                    new SuppressNavigationTransitionInfo());
-            }
-        }
-
-        private void MasterListView_OnItemClick(object sender,
-            ItemClickEventArgs e) {
-            var viewModel = (MainPageViewModel) this.DataContext;
-            viewModel.SelectedContact = (Contact) e.ClickedItem;
-
-            if (AdaptiveStates.CurrentState == NarrowState) {
-                Frame.Navigate(typeof(DetailPage), null,
-                    new DrillInNavigationTransitionInfo());
-            }
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
