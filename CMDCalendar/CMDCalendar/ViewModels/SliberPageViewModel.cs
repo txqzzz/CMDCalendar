b5bae49b7551900b2e382b1564691967846701ec
Chelsea <1612827756@qq.com>
2018-07-24 15:58:14 +0800

    merge ls todo

---
+++ CMDCalendar/CMDCalendar/ViewModels/SliberPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CMDCalendar.Database;
+using CMDCalendar.DB;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMDCalendar.ViewModels
+{
+    public class SliberPageViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 被选中的任务
+        /// </summary>
+        private DB.Task _selectedTask;
+        /// <summary>
+        /// 删除命令
+        /// </summary>
+        private  RelayCommand _deleteCommand;
+
+
+        public ObservableCollection<DB.Task> TaskCollection
+        {
+            get;
+            private set;
+        }
+
+
+        public DB.Task SelectedTask
+        {
+            get => _selectedTask;
+            set => Set(nameof(SelectedTask), ref _selectedTask, value);
+        }
+        public RelayCommand DeleteCommand(DB.Task) : IDatabaseUtils
+        {
+
+            return;
+        }
+
+        
+
+            
+    }
+}
