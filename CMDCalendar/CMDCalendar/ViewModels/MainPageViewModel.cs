68076babd0d15e6b067fc292c5f4a4d067648f12
txqzzz <txqzzz@hotmail.com>
2018-07-25 20:58:29 +0800

    add calendarpage viewmodel

---
+++ CMDCalendar/CMDCalendar/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CMDCalendar.Database;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace CMDCalendar.ViewModels
+{
+    /// <summary>
+    /// <viewmodel>MainPage_viewmodel</viewmodel>
+    /// </summary>
+    public class MainPageViewModel:ViewModelBase
+    {
+        /* private property*/
+
+        /// <summary>
+        /// get database service
+        /// </summary>
+        private IDatabaseUtils _databaseUtils;
+
+        private RelayCommand _refreshCommand;
+
+        /* public property */
+        
+
+
+    }
+}
